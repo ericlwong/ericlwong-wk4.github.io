724c5bb632856d645e020e03c4984d81a3bf3a0c
ericlwong <imericlwong@gmail.com>
2021-02-23 14:41:11 -0800

    Week 4 Assignment

---
+++ fancifymytext.js
@@ -0,0 +1,26 @@
+function biggerButton() {
+    alert("Hello, world!");
+    document.getElementById("textfield").style.fontSize = "2em";
+}
+
+function fancyButton() {
+    alert("Styles incoming!");
+    document.getElementById("textfield").style.fontWeight = "700";
+    document.getElementById("textfield").style.color = "blue";
+    document.getElementById("textfield").style.textDecoration = "underline";
+}
+
+function boringButton() {
+    document.getElementById("textfield").style.fontWeight = "400";
+    document.getElementById("textfield").style.color = "black";
+    document.getElementById("textfield").style.textDecoration = "none";
+}
+
+function mooButton() {
+    document.getElementById("textfield").style.textTransform = "uppercase";
+    var str = document.getElementById("textfield").value;
+    var parts = str.split(".");
+    str = parts.join("-Moo.");
+    document.getElementById("textfield").value = str;
+
+}
